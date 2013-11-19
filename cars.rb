fabe2735ed8ec0a0273cf2dbbf55e91f0371ea1d
chrisbodhi <cboette@gmail.com>
2013-11-18 21:56:38 -0600

    Add setter method to change color of car instance, update hash in class to keep count. Method to_s displays car color.

--- cars.rb
+++ cars.rb
@@ -1,13 +1,57 @@
 class Car
-	def to_s()
-		"I'm a car! I've driven #{@distance} and have #{@fuel} gallons gas left"
-	end
 
-	def initialize()
+	# Class variables that keep track of instance information
+	@@total_car_count = 0
+	@@car_colors = {}
+	@@most_popular_color = nil
+
+	# Sets the car color to white as a default, tank as full, resets the distance
+	# Increments the total car count and most popular color for the class
+	def initialize(color = "white")
 		@fuel = 10
 		@distance = 0
+		@color = color
+		@@total_car_count += 1
+		if @@car_colors.include?(color)
+			@@car_colors[color] += 1
+		else
+			@@car_colors.merge!(color => 1)
+		end
 	end
 
+	# The getter version of class for car count
+	def self.total_car_count
+		@@total_car_count
+	end
+
+	# Arranges the hash that has collected the count of car colors and updates
+	# the @@most_popular_color with the most popular color from an array of 
+	# the color and its count
+	def self.most_popular_color
+		popular = @@car_colors.max_by { |color, count| count }
+		@@most_popular_color = popular[0]
+		@@most_popular_color
+	end
+
+	# Updates the color of the car and the @@car_colors hash that keeps track 
+	# of color and count
+	def color=(new)
+		puts "The new color is #{new}."
+		if @@car_colors.include?(new)
+			@@car_colors[@color] -= 1
+			@@car_colors[new] += 1
+		else
+			@@car_colors.merge!(new => 1)
+		end
+		@color = new
+	end
+
+	# Displays the car instance's distance and fuel information when 'puts' is used
+	def to_s()
+		"I'm a #{@color} car! I've driven #{@distance} and have #{@fuel} gallons of gas left."
+	end
+
+	# Determines the gas used to drive the specified distance
 	def drive(miles)
 		if (@fuel - miles/20.0) >= 0
 			@distance += miles
@@ -19,6 +63,7 @@ class Car
 		end
 	end
 
+	# Calculated the remaining gallons and cost to fill up before filling tank.
 	def fuel_up()
 		gallons_needed = 10.0 - @fuel
 		puts "You must pay $#{3.5 * gallons_needed}"
@@ -26,14 +71,21 @@ class Car
 	end
 end
 
-car_a = Car.new()
-car_b = Car.new()
-puts car_a
-puts car_b
-car_a.drive(10)
-puts car_a
-puts car_b
-car_a.drive(232)
-car_b.drive(117)
-puts car_a
-puts car_b
+# car_a = Car.new('black')
+# car_b = Car.new('blue')
+# car_c = Car.new('black')
+car_d = Car.new
+puts car_d
+car_d.color = "blue"
+puts car_d
+puts Car.total_car_count
+puts Car.most_popular_color
+# p car_a
+# print car_b
+# car_a.drive(10)
+# puts car_a
+# puts car_b
+# car_a.drive(232)
+# car_b.drive(117)
+# puts car_a
+# puts car_b
